59a78a6b0a3aed1c0bea5bfa4ab3cfe9282c6fce
jiashuai <leaderjs@live.com>
2018-08-19 06:23:09 -0400

    forth commit

--- staticNet.sh
+++ staticNet.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Program:
-#     用来设置静态ip, 只适合RHEL或Centos。
-# 1、先检查发行版本，不支持的版本就退出
-# 2、再检查是否已经配置了静态ip，配了就退出
+#     当新建了一个本地虚拟机，来设置静态ip
 # History:
 # 2018/08/18 leaderjs First release
 
@@ -12,16 +10,8 @@ PATH=/bin:/sbin:/usr/bin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 # 把需要的脚本都复制到~/bin下
-cp ./*.sh ~/bin/
+#cp ./*.sh ~/bin/
 
-mynetall=$(ip route | sed -n '1p;1q')
-ifname=$(echo ${mynetall} | awk '{print $5}')
-# 判断网卡配置文件是否存在
-if [ ! -e "/etc/sysconfig/network-scripts/ifcfg-${ifname}" ]; then
-    echo "这个脚本只适合redhat系列的发行版，比如centos。"
-    ./Get_Dist_Name.sh
-    exit 1
-fi
 
 #grep -E "IPADDR|BOOTPROTO|ONBOOT"  /etc/sysconfig/network-scripts/ifcfg-${ifname}
 
@@ -40,7 +30,8 @@ mygateway=$(echo ${mynetall} | awk '{print $3}')
 ifname=$(echo ${mynetall} | awk '{print $5}')
 
 # 网卡配置文件
-netconf="/etc/sysconfig/network-scripts/ifcfg-${ifname}"
+netconf=$(find /etc/ -name "*${ifname}")
+echo "i find $netconf"
 
 # 修改网卡信息,第一个参数网卡的配置文件
 changeNet(){
@@ -55,28 +46,36 @@ EOF
     sed -i 's/BOOTPROTO=dhcp/BOOTPROTO=static/g' $1
     sed -i 's/ONBOOT=no/ONBOOT=yes/g' $1
     #判断有没有静态ip
+       sleep 0.5
     if grep -q IPADDR $1;then
         echo "1、you have static ip already"
     else
         echo -e "\\nIPADDR=${myip}" >> $1
+        echo "ip配置成功"
     fi
     #判断有没有子网掩码
+       sleep 0.5
     if grep -q NETMASK $1;then
         echo "2、you have NETMASK already"
     else
         echo -e "\\nNETMASK=${mynetmask}" >> $1
+        echo "netmask配置成功"
     fi
     #判断有没有网管
+       sleep 0.5 
     if grep -q GATEWAY $1;then
         echo "3、you have GATEWAY already"
     else
         echo -e "\\nGATEWAY=${mygateway}" >> $1
+        echo "gateway配置成功"
     fi
     #判断有没有dns
+       sleep 0.5
     if grep -q DNS $1;then
-        echo "3、you have DNS already"
+        echo "4、you have DNS already"
     else
         echo -e "\\nDNS1=${mygateway}" >> $1
+        echo "dns配置成功"
     fi
     cat <<EOF
 ################################################################################
